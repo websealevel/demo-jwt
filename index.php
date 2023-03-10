c942f07306c41957d68b81445ceaab42012e03b2
p. schuhmacher <contact@pschuhmacher.com>
2023-03-10 11:29:37 +0100

    met a jour le markup

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<nav>
-    <a href="edit-content.php">Éditer le contenu du site</a>
-</nav>
+<!DOCTYPE html>
+<html lang="fr">
 
-<h1>Bienvue</h1>
-<h2>Login</h2>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Démo JWT Token</title>
+</head>
 
-<form action="authentificate.php" method="POST">
-    <label>Login:</label><input type="text" name="login" />
-    <label>Password:</label><input type="password" name="password" />
-    <input type="submit" value="Sign in" />
-</form>
+<body>
+    <nav>
+        <a href="edit-content.php">Éditer le contenu du site</a>
+    </nav>
+
+    <h1>Bienvue</h1>
+    <h2>Login</h2>
+
+    <form action="authentificate.php" method="POST">
+        <label>Login:</label><input type="text" name="login" />
+        <label>Password:</label><input type="password" name="password" />
+        <input type=" submit" value="Sign in" />
+    </form>
+</body>
+
+</html>
